a91f53866abbd7b32bc20c29a09de9e6edb960c0
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2024-01-11 19:53:47 +0000

    Criação da interface gráfica para listar os alunos

--- src/main/java/example/m08_aula07_appdemo/StudentlistController.java
+++ src/main/java/example/m08_aula07_appdemo/StudentlistController.java
@@ -1,4 +1,28 @@
 package example.m08_aula07_appdemo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentlistController {
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn tableColumnNumber;
+    @FXML
+    private TableColumn tableColumnName;
+    @FXML
+    private TableColumn tableColumnAge;
+    @FXML
+    private TableColumn tableColumnGender;
+
+    public void buttonEdit(ActionEvent actionEvent) {
+    }
+
+    public void buttonDelete(ActionEvent actionEvent) {
+    }
+
+    public void buttonClose(ActionEvent actionEvent) {
+    }
 }
